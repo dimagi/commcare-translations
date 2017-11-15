83872f278d4c4d269d6987c79c3b8f1440011f10
Nick Pellegrino <npellegrino@dimagi.com>
2017-11-15 13:34:07 -0600

    python-modernize . -w -f libfuturize.fixes.fix_print_with_import

--- commcare_translations.py
+++ commcare_translations.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from __future__ import print_function
 from distutils.version import StrictVersion
 from os import listdir
 from os.path import join, normpath
@@ -91,5 +92,5 @@ def dumps(dct):
         val = val.replace('\n', '\\n')
         # escape starting # character
         val = re.sub(r'(?<!\\)#', '\#', val)
-        print >> io, u"{key}={val}".format(key=key.strip(), val=val).encode('utf8')
+        print(u"{key}={val}".format(key=key.strip(), val=val).encode('utf8'), file=io)
     return unicode(io.getvalue(), encoding='utf8')
